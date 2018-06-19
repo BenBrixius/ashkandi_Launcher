64173d5501848f30f389470dc39e009f3695f366
benbrixius <ben.brixius@live.com>
2018-06-19 09:46:09 -0500

    This is the first upload of this project.

---
+++ launcher2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace launcher2
+{
+    partial class launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(launcher));
+            this.txtRealm1 = new System.Windows.Forms.TextBox();
+            this.txtRealm2 = new System.Windows.Forms.TextBox();
+            this.txtRealm3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnPlay3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRealm1
+            // 
+            this.txtRealm1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtRealm1.Enabled = false;
+            this.txtRealm1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRealm1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtRealm1.Location = new System.Drawing.Point(99, 34);
+            this.txtRealm1.Name = "txtRealm1";
+            this.txtRealm1.Size = new System.Drawing.Size(625, 42);
+            this.txtRealm1.TabIndex = 0;
+            this.txtRealm1.Text = "set realmlist logon.lightshope.org";
+            // 
+            // txtRealm2
+            // 
+            this.txtRealm2.Enabled = false;
+            this.txtRealm2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRealm2.Location = new System.Drawing.Point(99, 153);
+            this.txtRealm2.Name = "txtRealm2";
+            this.txtRealm2.Size = new System.Drawing.Size(625, 42);
+            this.txtRealm2.TabIndex = 1;
+            // 
+            // txtRealm3
+            // 
+            this.txtRealm3.Enabled = false;
+            this.txtRealm3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRealm3.Location = new System.Drawing.Point(99, 276);
+            this.txtRealm3.Name = "txtRealm3";
+            this.txtRealm3.Size = new System.Drawing.Size(625, 42);
+            this.txtRealm3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(741, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 80);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 79);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnPlay3
+            // 
+            this.btnPlay3.Location = new System.Drawing.Point(741, 259);
+            this.btnPlay3.Name = "btnPlay3";
+            this.btnPlay3.Size = new System.Drawing.Size(158, 79);
+            this.btnPlay3.TabIndex = 5;
+            this.btnPlay3.Text = "Play";
+            this.btnPlay3.UseVisualStyleBackColor = true;
+            this.btnPlay3.Click += new System.EventHandler(this.btnPlay3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(61, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(61, 166);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 21);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(61, 289);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(22, 21);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(714, 371);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(185, 44);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save Changes";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Info";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(950, 455);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnPlay3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRealm3);
+            this.Controls.Add(this.txtRealm2);
+            this.Controls.Add(this.txtRealm1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Custom WoW Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRealm1;
+        private System.Windows.Forms.TextBox txtRealm2;
+        private System.Windows.Forms.TextBox txtRealm3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPlay3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ launcher2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace launcher2
+{
+    public partial class launcher : Form
+    {
+        string path = Path.GetDirectoryName(Application.ExecutablePath) + "\\realms.txt";
+
+
+        public launcher()
+        {
+            InitializeComponent();
+
+            if (!File.Exists(path)) 
+            {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path)) 
+            {
+                sw.WriteLine("set realmlist logon.lightshope.org" + "\r\n" + "n/a" + "\r\n" + "n/a");
+            }	
+            } else
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                    txtRealm1.Text = lines[0];
+                    txtRealm2.Text = lines[1];
+                    txtRealm3.Text = lines[2];
+                
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtRealm1.Enabled == true)
+            {
+                txtRealm1.Enabled = false;
+            } else
+            {
+                txtRealm1.Enabled = true;
+            }
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtRealm2.Enabled == true)
+            {
+                txtRealm2.Enabled = false;
+            }
+            else
+            {
+                txtRealm2.Enabled = true;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtRealm3.Enabled == true)
+            {
+                txtRealm3.Enabled = false;
+            }
+            else
+            {
+                txtRealm3.Enabled = true;
+            }
+        }
+
+        public void writeRealmList(string a)
+        {
+            File.WriteAllText("realmlist.wtf", a);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeRealmList(txtRealm1.Text);
+            Process.Start("WoW.exe");
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeRealmList(txtRealm2.Text);
+            Process.Start("WoW.exe");
+            Close();
+        }
+
+        private void btnPlay3_Click(object sender, EventArgs e)
+        {
+            writeRealmList(txtRealm3.Text);
+            Process.Start("WoW.exe");
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            sw.WriteLine(txtRealm1.Text + "\r\n" + txtRealm2.Text + "\r\n" + txtRealm3.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Select the checkbox to unlock and lock the realmlist entry. \r\rPressing save changes will keep the list up to date if you change them. \r\rPressing play will automatically change your realmlist, close the launcher, and open WoW. \r\rData is kept in realms.txt in same directory.", "Halp Window");
+        }
+    }
+}
